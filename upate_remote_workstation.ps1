9d129f86d40d7a1b0fd3f5922f342b7b9a8f94bf
Roger Poore <rpoore@gmail.com>
2018-06-28 16:23:16 -0400

    update

--- upate_remote_workstation.ps1
+++ upate_remote_workstation.ps1
@@ -10,6 +10,10 @@ param (
 $net = $IPAddress.Substring(0, $IPAddress.LastIndexOf('.') + 1)
 $gw = $net + '1'
 
+# get the remote host for the requestor
+#   I had this info in a MySQL table
+#   you'll have to come up with your own way to get this
+
 $script = "New-NetIPAddress –InterfaceAlias "“Primary”" –IPv4Address $IPAddress –PrefixLength 24 -DefaultGateway $gw"
 
 Invoke-Command -ComputerName $Remhost -ScriptBlock { Start-Job -ScriptBlock { $script } }
